8f92b23171ca84ff2a9411e35e7608a0a2468269
ChandruZethic <chandru@zethic.com>
2024-09-02 18:55:55 +0530

    Navbar and tailwind configs

---
+++ src/components/Navbar/NavLinks.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NavLinks = ({ link, onClick }) => (
+  <Link to={link.path} onClick={onClick}>
+    <p
+      className={`border-whiteColor rounded-lg border-2 px-4 py-1 text-whiteColor font-medium uppercase hover:text-primaryColor hover:bg-secondaryColor hover:border-secondaryColor cursor-pointer ease-in-out duration-150 `}
+    >
+      {link.title}
+    </p>
+  </Link>
+);
+
+export default NavLinks;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import NavLinks from "./NavLinks";
+
+export default function Navbar() {
+  const navLinks = [
+    {
+      title: "About",
+      path: "/about",
+    },
+    {
+      title: "Projects",
+      path: "/projects",
+    },
+    {
+      title: "Contact",
+      path: "/contact",
+    },
+  ];
+
+  return (
+    <nav className="w-full z-50 top-0">
+      <div className="h-full max-w-screen-xl mx-auto flex flex-row items-center justify-between px-4 py-4">
+        <div className="text-secondaryColor uppercase text-lg font-semibold">
+          Chandru
+        </div>
+        <div className="block w-auto" id="navbar">
+          <ul className="flex items-center p-4 md:p-0 md:flex-row md:space-x-8 space-x-2 mt-0">
+            {navLinks.map((navLink, index) => (
+              <li key={index}>
+                <NavLinks link={navLink} onClick={() => {}} />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
